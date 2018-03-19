7f1b22886f632da16599822db7190ae55268aaeb
Rossnerr <metei1997vasile@gmail.com>
2018-03-19 09:34:16 +0200

    first commit

---
+++ WT_Lab.2/eUseControl/eUseControl.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Optimization;
+
+namespace eUseControl.Web
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //Home style
+            bundles.Add(new StyleBundle("~/bundles/main/css").Include(
+                "~/Content/file.css", new CssRewriteUrlTransform()));
+
+            //Bootstrap style
+            bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include(
+                "~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
+
+            //Bootstrap
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap/js").Include(
+                "~/Scripts/bootstrap.min.js"));
+
+            //jQuery
+            bundles.Add(new ScriptBundle("~/bundles/jquery/js").Include(
+                "~/Scripts/jquery-3.3.1.min.js"));
+        }
+    }
+}
